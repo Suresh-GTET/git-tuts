517aaadc9b3c94be7d7c280541559a00439d71b4
Suresh <gundupalli.suresh@narayanagroup.com>
2025-05-05 14:49:20 +0530

    commited modified demo file

--- Demo.java
+++ Demo.java
@@ -1,6 +1,8 @@
 class Demo{
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Another line addded");
+        System.out.println("Added from git hub");
+        //i have added the above line i dont know why it is not update
+        System.out.println("Added another line to git hub from vs code");
     }
 }
